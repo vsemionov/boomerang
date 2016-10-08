407b343ab0911ba813e3fcf5732287194bffdb81
Victor Semionov <vsemionov@gmail.com>
2016-10-09 01:51:53 +0300

    models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Notebook, Note, Task
+
+admin.site.register(Notebook)
+admin.site.register(Note)
+admin.site.register(Task)
--- api/models.py
+++ api/models.py
@@ -3,3 +3,21 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Notebook(models.Model):
+    user = models.ForeignKey("auth.User")
+    name = models.CharField(max_length=128)
+
+
+class Note(models.Model):
+    notebook = models.ForeignKey(Notebook)
+    title = models.CharField(max_length=128)
+    text = models.TextField()
+
+
+class Task(models.Model):
+    user = models.ForeignKey("auth.User", editable=False)
+    done = models.BooleanField()
+    title = models.CharField(max_length=128)
+    description = models.TextField()
