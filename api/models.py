02c339c04eee6d1cd10ea032b0ef2187b24ce2e4
Victor Semionov <vsemionov@gmail.com>
2016-10-09 14:04:57 +0300

    updated models

--- api/models.py
+++ api/models.py
@@ -4,20 +4,22 @@ from django.db import models
 
 # Create your models here.
 
+MAX_NAME_SIZE = 128
+MAX_TEXT_SIZE = 32 * 1024
 
 class Notebook(models.Model):
     user = models.ForeignKey("auth.User")
-    name = models.CharField(max_length=128)
+    name = models.CharField(max_length=MAX_NAME_SIZE)
 
 
 class Note(models.Model):
     notebook = models.ForeignKey(Notebook)
-    title = models.CharField(max_length=128)
-    text = models.TextField()
+    title = models.CharField(max_length=MAX_NAME_SIZE)
+    text = models.TextField(max_length=MAX_TEXT_SIZE)
 
 
 class Task(models.Model):
-    user = models.ForeignKey("auth.User", editable=False)
-    done = models.BooleanField()
-    title = models.CharField(max_length=128)
-    description = models.TextField()
+    user = models.ForeignKey("auth.User")
+    done = models.BooleanField(default=False)
+    title = models.CharField(max_length=MAX_NAME_SIZE)
+    description = models.TextField(max_length=MAX_TEXT_SIZE)
