e922bc062f266a204051b98e868a2c2e05b705a9
Victor Semionov <vsemionov@gmail.com>
2016-10-09 20:08:27 +0300

    no admin text limits model object names

--- api/models.py
+++ api/models.py
@@ -5,22 +5,30 @@ from django.db import models
 # Create your models here.
 
 MAX_NAME_SIZE = 128
-MAX_TEXT_SIZE = 32 * 1024
 
 
 class Notebook(models.Model):
     user = models.ForeignKey("auth.User")
     name = models.CharField(max_length=MAX_NAME_SIZE)
 
+    def __unicode__(self):
+        return self.name
+
 
 class Note(models.Model):
     notebook = models.ForeignKey(Notebook)
     title = models.CharField(max_length=MAX_NAME_SIZE)
-    text = models.TextField(max_length=MAX_TEXT_SIZE)
+    text = models.TextField()
+
+    def __unicode__(self):
+        return self.title
 
 
 class Task(models.Model):
     user = models.ForeignKey("auth.User")
     done = models.BooleanField(default=False)
     title = models.CharField(max_length=MAX_NAME_SIZE)
-    description = models.TextField(max_length=MAX_TEXT_SIZE)
+    description = models.TextField()
+
+    def __unicode__(self):
+        return self.title
