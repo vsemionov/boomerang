28e139141fb101bc4ff10176e1569437c597dcec
Victor Semionov <vsemionov@gmail.com>
2016-10-09 14:27:55 +0300

    blank line

--- api/models.py
+++ api/models.py
@@ -7,6 +7,7 @@ from django.db import models
 MAX_NAME_SIZE = 128
 MAX_TEXT_SIZE = 32 * 1024
 
+
 class Notebook(models.Model):
     user = models.ForeignKey("auth.User")
     name = models.CharField(max_length=MAX_NAME_SIZE)
