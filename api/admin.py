795a2c729c41beb1ecd737777510f5e7d387fe8c
Victor Semionov <vsemionov@gmail.com>
2016-10-26 06:47:09 +0300

    fixed admin site models were registered with django.contrib.admin.site, instead of the custom site

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+
+admin.site.site_header = 'Notes administration'
+admin.site.site_title = 'Notes site admin'
--- notes/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-
-class AdminSite(admin.AdminSite):
-    site_header = 'Notes administration'
-    site_title = 'Notes site admin'
-
-
-site = AdminSite()
