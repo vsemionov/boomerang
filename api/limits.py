bb4605d391561f34ea6f70eeb1cb69ae3e063e43
Victor Semionov <vsemionov@gmail.com>
2016-10-29 15:49:41 +0300

    return http 402 on exceeded limits

--- api/limits.py
+++ api/limits.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from rest_framework import exceptions
+from rest_framework import exceptions, status
 from .models import Notebook, Note, Task
 
 
@@ -14,6 +14,11 @@ LIMITS = {
 }
 
 
+class LimitExceededError(exceptions.APIException):
+    status_code = status.HTTP_402_PAYMENT_REQUIRED
+    default_detail = 'limit exceeded'
+
+
 def check_limits(parent, child_type):
     parent_limits = LIMITS.get(parent.__class__)
     if not parent_limits:
@@ -26,5 +31,5 @@ def check_limits(parent, child_type):
     child_set_name = child_type._meta.verbose_name + '_set'
     child_set = getattr(parent, child_set_name)
     if child_set.count() >= limit:
-        raise exceptions.PermissionDenied('can not create more than %d %s per %s' % \
-                                          (limit, child_type._meta.verbose_name_plural, parent._meta.verbose_name))
+        raise LimitExceededError('exceeded limit of %d %s per %s' % \
+                                 (limit, child_type._meta.verbose_name_plural, parent._meta.verbose_name))
