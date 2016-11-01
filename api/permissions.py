2d5f55efea88ed24de056c8c0b0f1461c359401e
Victor Semionov <vsemionov@gmail.com>
2016-11-01 21:14:16 +0200

    refactoring

--- api/permissions.py
+++ api/permissions.py
@@ -3,12 +3,11 @@ from rest_framework import permissions
 
 class UserPermissions(permissions.BasePermission):
     def has_permission(self, request, view):
-        if view.action == 'retrieve':
+        if view.action == 'list':
+            return True
+        else:
             return str(request.user.username) == view.kwargs['username'] or \
                    (request.user.is_staff and request.method in permissions.SAFE_METHODS)
-        else:
-            # list
-            return True
 
 
 class NestedPermissions(permissions.BasePermission):
