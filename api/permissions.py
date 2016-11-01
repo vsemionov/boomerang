9e09c498472e78f1a3a63b92f8834f0172a0e6e0
Victor Semionov <vsemionov@gmail.com>
2016-11-01 23:14:48 +0200

    Revert "refactoring"
    This reverts commit 2d5f55efea88ed24de056c8c0b0f1461c359401e.

--- api/permissions.py
+++ api/permissions.py
@@ -3,11 +3,12 @@ from rest_framework import permissions
 
 class UserPermissions(permissions.BasePermission):
     def has_permission(self, request, view):
-        if view.action == 'list':
-            return True
-        else:
+        if view.action == 'retrieve':
             return str(request.user.username) == view.kwargs['username'] or \
                    (request.user.is_staff and request.method in permissions.SAFE_METHODS)
+        else:
+            # list
+            return True
 
 
 class NestedPermissions(permissions.BasePermission):
