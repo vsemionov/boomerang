b671030059d8f155567eebe3ccf766c26c9d5e21
Victor Semionov <vsemionov@gmail.com>
2017-03-19 20:47:53 +0200

    explanatory output

--- bin/maintenance.sh
+++ bin/maintenance.sh
@@ -2,5 +2,8 @@
 
 BASE_DIR=$(dirname $(dirname $0))
 
+echo "clearing expired sessions"
 python $BASE_DIR/manage.py clearsessions
+
+echo "clearing expired deleted objects"
 python $BASE_DIR/manage.py cleardeleted
