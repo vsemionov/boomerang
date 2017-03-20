8cf7b0c04f2ac5bc94f50ad638a94c573a535238
Victor Semionov <vsemionov@gmail.com>
2017-03-20 03:33:23 +0200

    corrected server email

--- boomerang/settings.py
+++ boomerang/settings.py
@@ -146,10 +146,10 @@ STATIC_URL = '/static/'
 # Email
 
 ADMIN_NAME = 'Boomerang Admin'
-SERVER_EMAIL = 'boomerang-notes@gmail.com'
+SERVER_EMAIL = 'boomerang.notes@gmail.com'
 DEFAULT_FROM_EMAIL = '%s <%s>' % (ADMIN_NAME, SERVER_EMAIL)
 
-EMAIL_HOST_USER = 'boomerang-notes@gmail.com'
+EMAIL_HOST_USER = 'boomerang.notes@gmail.com'
 EMAIL_HOST_PASSWORD = 'noteslow'
 
 EMAIL_HOST = 'smtp.gmail.com'
