4278daf8a48d73bd3948403eb2301a2949216837
Victor Semionov <vsemionov@gmail.com>
2017-03-31 01:15:33 +0300

    regard postgresql_psycopg2 as postgresql, fixes fuzzy search on heroku

--- api/util.py
+++ api/util.py
@@ -2,7 +2,7 @@ from django.conf import settings
 
 
 def is_pgsql():
-    return settings.DATABASES['default']['ENGINE'] == 'django.db.backends.postgresql'
+    return settings.DATABASES['default']['ENGINE'].startswith('django.db.backends.postgresql')
 
 def get_view_description(cld, html=False):
     return ''
