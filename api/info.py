73603ab9b4b13f8a7166fc94518f8f496bda7285
Victor Semionov <vsemionov@gmail.com>
2017-03-31 23:33:22 +0300

    version 0.5.15

--- api/info.py
+++ api/info.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets, mixins, response, reverse
 
 
 NAME = 'vsemionov.boomerang.api'
-VERSION = '0.5.14'
+VERSION = '0.5.15'
 
 
 class ApiInfoViewSet(mixins.ListModelMixin,
