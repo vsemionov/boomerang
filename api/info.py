bb30e17a99eff6712e0c23af7cb95c924ea27b83
Victor Semionov <vsemionov@gmail.com>
2017-04-16 01:29:15 +0300

    return null user if not logged in

--- api/info.py
+++ api/info.py
@@ -12,7 +12,7 @@ class ApiInfoViewSet(mixins.ListModelMixin,
 
     @staticmethod
     def _get_user_url(request):
-        return request.user.id and reverse.reverse('user-detail', request=request, args=[request.user.username])
+        return reverse.reverse('user-detail', request=request, args=[request.user.username])
 
     def get_view_name(self):
         return self.view_name
@@ -21,7 +21,8 @@ class ApiInfoViewSet(mixins.ListModelMixin,
         app = OrderedDict((('name', NAME),
                            ('version', VERSION)))
         user = OrderedDict((('username', request.user.username),
-                            ('url', self._get_user_url(request))))
+                            ('url', self._get_user_url(request)))) \
+            if request.user.id else None
         info = OrderedDict((('app', app),
                             ('user', user)))
         return response.Response(info)
