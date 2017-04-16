6778c8a3bff65c0cc5cf8c2edd1590ea2ab42c7a
Victor Semionov <vsemionov@gmail.com>
2017-04-16 14:16:48 +0300

    important comment about global throttle cache

--- api/throttle.py
+++ api/throttle.py
@@ -5,6 +5,9 @@ from rest_framework import throttling
 THROTTLE_CACHE_ALIAS = 'api_throttle'
 
 
+# django caches are thread local, including their connection pools
+# therefore, different threads cannot reuse each other's connections
+# this cache and its connection pool are created at import time and are therefore global
 throttle_cache = caches[THROTTLE_CACHE_ALIAS]
 
 
