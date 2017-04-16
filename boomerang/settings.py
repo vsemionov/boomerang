2c81c785856130e1bfa38a9604113214290037ab
Victor Semionov <vsemionov@gmail.com>
2017-04-16 11:57:06 +0300

    alternative workaround for the fbsr_ cookie problem - unset the cookie in a middleware

--- boomerang/settings.py
+++ boomerang/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'boomerang.nofbsr.NoFbsrMiddleware', # TODO: remove this when django-allauth issue #1701 is fixed; see code
 ]
 
 ROOT_URLCONF = 'boomerang.urls'
