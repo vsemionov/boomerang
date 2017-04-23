0e437b3f040cd7da86c4ac93aa01bdf803dacd8f
Victor Semionov <vsemionov@gmail.com>
2017-04-23 23:43:21 +0300

    enable production mode for remote hosts

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,5 +1,12 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {enableProdMode}         from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+import {AppModule} from './app/app.module';
+
+
+// Enable production mode unless running locally
+if (!/^localhost(:\d+)?$/.test(document.location.host) && !/^127\.0\.0\.1(:\d+)?$/.test(document.location.host)) {
+    enableProdMode();
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule);
