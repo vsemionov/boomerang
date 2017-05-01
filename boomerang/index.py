8eec8b5b33505dae83a477ffa30ea4dc907f202f
Victor Semionov <vsemionov@gmail.com>
2017-05-02 00:13:10 +0300

    serve index.html from app dirs in debug mode

--- boomerang/index.py
+++ boomerang/index.py
@@ -2,13 +2,18 @@ import os
 
 from django.conf import settings
 from django.http import FileResponse
+from django.contrib.staticfiles import finders
 
 from api.util import create_jwt
 
 
 def index(request):
-    path = os.path.join(settings.STATIC_ROOT, 'web', 'index.html')
-    response = FileResponse(open(path, 'rb'), content_type='text/html; charset=utf-8')
+    relpath = os.path.join('web', 'index.html')
+    if settings.DEBUG:
+        abspath = finders.find(relpath)
+    else:
+        abspath = os.path.join(settings.STATIC_ROOT, relpath)
+    response = FileResponse(open(abspath, 'rb'), content_type='text/html; charset=utf-8')
     if request.user and request.user.is_authenticated:
         token = create_jwt(request.user)
         response.set_cookie('jwt', token)
