6249436f4e2f1e357e6bb79545b788204d7ffcbf
Victor Semionov <vsemionov@gmail.com>
2017-08-19 16:51:43 +0300

    version 0.6.6

--- boomerang/settings.py
+++ boomerang/settings.py
@@ -16,7 +16,7 @@ import datetime
 import dj_database_url
 
 PROJECT_NAME = 'vsemionov.boomerang.api'
-PROJECT_VERSION = '0.6.5'
+PROJECT_VERSION = '0.6.6'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
