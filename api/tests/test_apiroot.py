06713eda01319bdf70bf1f77945ad582da638b63
Victor Semionov <vsemionov@gmail.com>
2017-08-27 02:46:32 +0300

    api root test

---
+++ api/tests/test_apiroot.py
@@ -0,0 +1,46 @@
+import urllib
+
+from django.urls import reverse
+from rest_framework.test import APITestCase, RequestsClient
+from rest_framework import status
+
+
+class TestApiRoot(APITestCase):
+
+    url = reverse('api-root')
+
+    def test_allows_unauthenticated(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_only_get(self):
+        response = RequestsClient().options('http://testserver' + self.url)
+        self.assertEqual(response.headers['Allow'], 'GET, HEAD, OPTIONS')
+
+    def test_post_fails(self):
+        response = self.client.post(self.url)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_put_fails(self):
+        response = self.client.put(self.url)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_patch_fails(self):
+        response = self.client.patch(self.url)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_delete_fails(self):
+        response = self.client.delete(self.url)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_has_users_link(self):
+        data = self.client.get(self.url).data
+        self.assertEqual(urllib.parse.urlparse(data['users']).path, reverse('user-list'))
+
+    def test_has_info_link(self):
+        data = self.client.get(self.url).data
+        self.assertEqual(urllib.parse.urlparse(data['info']).path, reverse('info-list'))
+
+    def test_has_jwt_link(self):
+        data = self.client.get(self.url).data
+        self.assertEqual(urllib.parse.urlparse(data['jwt']).path, reverse('jwt-list'))
--- api/tests/test_empty.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-
-
-class Empty(TestCase):
-
-    def test_empty(self):
-        pass
