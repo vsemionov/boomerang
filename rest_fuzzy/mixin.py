9fe3c69017ff4c49d800c93356be666442074d2d
Victor Semionov <vsemionov@gmail.com>
2017-08-29 15:22:28 +0300

    prevent rest_fuzzy to prevent filtering the queryset by ending the get_queryset() chain

--- rest_fuzzy/mixin.py
+++ rest_fuzzy/mixin.py
@@ -3,9 +3,6 @@ from rest_framework import response
 
 class ViewSetMixin(object):
 
-    def get_queryset(self):
-        return self.queryset
-
     def decorated_list(self, cls, context, request, *args, **kwargs):
         base_response = super(cls, self).list(request, *args, **kwargs)
 
