96c62adabe206c098b84ba741c19d073bc569451
Victor Semionov <vsemionov@gmail.com>
2017-08-30 22:25:46 +0300

    modify the base response object in place

--- rest_fuzzy/mixin.py
+++ rest_fuzzy/mixin.py
@@ -1,19 +1,19 @@
 from collections import OrderedDict
 
-from rest_framework import response
-
 
 class ViewSetMixin(object):
 
     def decorated_list(self, cls, context, request, *args, **kwargs):
-        base_response = super(cls, self).list(request, *args, **kwargs)
+        response = super(cls, self).list(request, *args, **kwargs)
 
-        if isinstance(base_response.data, dict):
-            base_data = base_response.data
+        if isinstance(response.data, dict):
+            base_data = response.data
         else:
-            base_data = OrderedDict(results=base_response.data)
+            base_data = OrderedDict(results=response.data)
 
         data = context.copy()
         data.update(base_data)
 
-        return response.Response(data)
+        response.data = data
+
+        return response
--- rest_offline/mixin.py
+++ rest_offline/mixin.py
@@ -1,7 +1,5 @@
 from collections import OrderedDict
 
-from rest_framework import response
-
 
 class ViewSetMixin(object):
 
@@ -9,14 +7,16 @@ class ViewSetMixin(object):
         return self.queryset
 
     def decorated_list(self, cls, context, request, *args, **kwargs):
-        base_response = super(cls, self).list(request, *args, **kwargs)
+        response = super(cls, self).list(request, *args, **kwargs)
 
-        if isinstance(base_response.data, dict):
-            base_data = base_response.data
+        if isinstance(response.data, dict):
+            base_data = response.data
         else:
-            base_data = OrderedDict(results=base_response.data)
+            base_data = OrderedDict(results=response.data)
 
         data = context.copy()
         data.update(base_data)
 
-        return response.Response(data)
+        response.data = data
+
+        return response
