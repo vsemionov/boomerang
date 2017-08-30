6629c5755edb4bf64e61cb1ff2026cf461671cb0
Victor Semionov <vsemionov@gmail.com>
2017-08-30 22:10:34 +0300

    if the returned results are a list, as is the case without configured pagination, convert them to a dict

--- rest_offline/mixin.py
+++ rest_offline/mixin.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 from rest_framework import response
 
 
@@ -9,7 +11,12 @@ class ViewSetMixin(object):
     def decorated_list(self, cls, context, request, *args, **kwargs):
         base_response = super(cls, self).list(request, *args, **kwargs)
 
+        if isinstance(base_response.data, dict):
+            base_data = base_response.data
+        else:
+            base_data = OrderedDict(results=base_response.data)
+
         data = context.copy()
-        data.update(base_response.data)
+        data.update(base_data)
 
         return response.Response(data)
