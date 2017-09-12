8502f96a979380fefc2efe6b8fd3a1d720b18323
Victor Semionov <vsemionov@gmail.com>
2017-09-13 02:40:12 +0300

    prevent refreshing JWTs

--- api/jwt.py
+++ api/jwt.py
@@ -1,11 +1,13 @@
 from collections import OrderedDict
 
 from rest_framework import viewsets, mixins, permissions, response, reverse
-from rest_framework_jwt.settings import api_settings
+from rest_framework.settings import api_settings as rest_settings
+from rest_framework_jwt.settings import api_settings as jwt_settings
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 
-jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
-jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+jwt_payload_handler = jwt_settings.JWT_PAYLOAD_HANDLER
+jwt_encode_handler = jwt_settings.JWT_ENCODE_HANDLER
 
 
 def create_jwt(user):
@@ -17,6 +19,8 @@ def create_jwt(user):
 class JWTViewSet(mixins.ListModelMixin,
                  viewsets.GenericViewSet):
     view_name = 'JWT'
+    authentication_classes = tuple(cls for cls in rest_settings.DEFAULT_AUTHENTICATION_CLASSES
+                                   if cls is not JSONWebTokenAuthentication)
     permission_classes = (permissions.IsAuthenticated,)
 
     @staticmethod
